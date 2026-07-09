020640d4a8edd7df93c21d859037abb8b0fcd25e
IvanKosovych <ivan95kos@gmail.com>
2026-07-09 12:26:53 +0200

    Initial commit: Spring Boot 4.1 skeleton for order-service

---
+++ src/test/java/com/ivankos/orderservice/OrderProcessingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivankos.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderProcessingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
