dd059cd98a323d1a671307e7817636dac674034c
IvanKosovych <ivan95kos@gmail.com>
2026-07-13 16:02:17 +0200

    Add ProblemDetail exception handling (RestControllerAdvice + problemdetails property)

---
+++ order-service/src/main/java/com/ivankos/orderservice/config/GlobalExceptionHandler.java
@@ -0,0 +1,16 @@
+package com.ivankos.orderservice.config;
+
+import com.ivankos.orderservice.exception.OrderNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(OrderNotFoundException.class)
+    public ProblemDetail handleOrderNotFoundException(OrderNotFoundException e) {
+        return ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, e.getMessage());
+    }
+}
